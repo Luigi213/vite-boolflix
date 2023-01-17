d9996676d2d9416d4b0ee3584eac03ed8c425fed
Luigi213 <reyesgianluigi88@gmail.com>
2023-01-17 13:15:53 +0100

    put list with movie features

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,12 +1,31 @@
 <script>
+import { store } from '../store.js'
 export default {
-
+    data(){
+        return{
+            store
+        }
+    }
 }
 </script>
 <template lang="">
-    <div>
+    <div class="container">
+        <ul>
+            <li v-for="(movie, index) in store.moviesArray" :key="index">
+                <h4>Titolo: {{ movie.title }}</h4> 
+                <h5>Titolo Originale: {{ movie.original_title}}</h5> 
+                <p>Lingua: {{ movie.original_language }}</p> 
+                <p>Voto: {{ movie.vote_average }}</p>
+            </li>
+        </ul>
     </div>
 </template>
 <style lang="scss" scoped>
-    
+    .container{
+        ul{
+            li{
+                margin: 10px 0;
+            }
+        }
+    }
 </style>
