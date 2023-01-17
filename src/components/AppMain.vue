6b6522d52e389edbad84f611a251510e617da6ff
Luigi213 <reyesgianluigi88@gmail.com>
2023-01-17 17:15:52 +0100

    put flags on series

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -3,9 +3,23 @@ import { store } from '../store.js'
 export default {
     data(){
         return{
-            store
+            store,
+            lang: store,
         }
     },
+    computed:{
+        country(){  
+            let newLang = this.lang.moviesArray
+            newLang.filter((elem) => {
+                return store.ciao = elem
+            })
+        },
+        ciao(){
+
+        }
+    },
+    methods:{
+    }
 }
 </script>
 <template lang="">
@@ -16,7 +30,9 @@ export default {
                     <li v-for="(movie, index) in store.moviesArray" :key="index">
                         <h4>Titolo: {{ movie.title }}</h4> 
                         <h5>Titolo Originale: {{ movie.original_title}}</h5> 
-                        <p>Lingua: {{ movie.original_language }}</p> 
+                        <div class="flag">
+                            Lingua:  <img :src="`https://flagsapi.com/${country}/flat/16.png`">
+                        </div>
                         <p>Voto: {{ movie.vote_average }}</p>
                     </li>
                 </ul>
@@ -26,7 +42,12 @@ export default {
                     <li v-for="(serie, index) in store.seriesArray" :key="index">
                         <h4>Titolo: {{ serie.name }}</h4> 
                         <h5>Titolo Originale: {{ serie.original_title}}</h5> 
-                        <p>Lingua: {{ serie.original_language }}</p> 
+                        <div class="flag" v-if="serie.origin_country[0] != undefined">
+                            Lingua:  <img :src="`https://flagsapi.com/${serie.origin_country[0]}/flat/16.png`">
+                        </div> 
+                        <div class="flag" v-else>
+                            Lingua:  <img src="https://flagsapi.com/US/shiny/16.png">
+                        </div>
                         <p>Voto: {{ serie.vote_average }}</p>
                     </li>
                 </ul>
@@ -42,6 +63,9 @@ export default {
             ul{
                 li{
                     margin: 10px 0;
+                    .flag{
+                        display: flex;
+                    }
                 }
             }
         }
